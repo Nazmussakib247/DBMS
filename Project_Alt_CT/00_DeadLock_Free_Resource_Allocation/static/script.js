ec658c74030708556653792546c3f45b25adfa90
Nazmus Sakib <nazmuss247@gmail.com>
2025-02-23 19:35:33 +0600

    A minor update

--- Project_Alt_CT/00_DeadLock_Free_Resource_Allocation/static/script.js
+++ Project_Alt_CT/00_DeadLock_Free_Resource_Allocation/static/script.js
@@ -4,16 +4,24 @@ function generateTable() {
     let tablesDiv = document.getElementById("tables");
     tablesDiv.innerHTML = '';
 
-    let availableTable = `<h3>Available Resources</h3><table><tr>`;
+    let availableTable = `<h3>Available Resources</h3><table><tr><th></th>`;
+    for (let j = 0; j < numResources; j++) {
+        availableTable += `<th><strong>Resource ${j + 1} ↓</strong></th>`;
+    }
+    availableTable += `</tr><tr><td><strong>Available</strong></td>`;
     for (let j = 0; j < numResources; j++) {
         availableTable += `<td><input type="number" id="available_${j}" value="0"></td>`;
     }
     availableTable += `</tr></table>`;
     tablesDiv.innerHTML += availableTable;
 
-    let maxNeedTable = `<h3>Max Need</h3><table>`;
+    let maxNeedTable = `<h3>Max Need</h3><table><tr><th></th>`;
+    for (let j = 0; j < numResources; j++) {
+        maxNeedTable += `<th><strong>Resource ${j + 1} ↓</strong></th>`;
+    }
+    maxNeedTable += `</tr>`;
     for (let i = 0; i < numProcesses; i++) {
-        maxNeedTable += `<tr>`;
+        maxNeedTable += `<tr><td><strong>Process ${i + 1} →</strong></td>`;
         for (let j = 0; j < numResources; j++) {
             maxNeedTable += `<td><input type="number" id="max_${i}_${j}" value="0"></td>`;
         }
@@ -22,9 +30,13 @@ function generateTable() {
     maxNeedTable += `</table>`;
     tablesDiv.innerHTML += maxNeedTable;
 
-    let allocatedTable = `<h3>Allocated</h3><table>`;
+    let allocatedTable = `<h3>Allocated</h3><table><tr><th></th>`;
+    for (let j = 0; j < numResources; j++) {
+        allocatedTable += `<th><strong>Resource ${j + 1} ↓</strong></th>`;
+    }
+    allocatedTable += `</tr>`;
     for (let i = 0; i < numProcesses; i++) {
-        allocatedTable += `<tr>`;
+        allocatedTable += `<tr><td><strong>Process ${i + 1} →</strong></td>`;
         for (let j = 0; j < numResources; j++) {
             allocatedTable += `<td><input type="number" id="alloc_${i}_${j}" value="0"></td>`;
         }
